c7b0cc7a8698b960be3a9932645039d8e0339d74
Djowood <jolan.cornevin@insa-lyon.fr>
2016-09-23 07:26:00 +0200

    add source project

---
+++ app/src/main/java/fr/insa/ot3/models/Drawing.java
@@ -0,0 +1,30 @@
+package geodrawmodel;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Stocks a list of drawable segments
+ * @author Max 
+ */
+public class Drawing {
+    private final List<Segment> trace;
+
+    public Drawing()
+    {
+        this.trace = new LinkedList<>();
+    }
+    
+    public void addSegment(Segment segment)
+    {
+        trace.add(segment);
+    }
+
+    public List<Segment> getSegments()
+    {
+        return trace;
+    }
+    
+    
+    
+}
