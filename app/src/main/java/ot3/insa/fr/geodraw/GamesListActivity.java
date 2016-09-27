49dcd2def8051488e681eb2fc9cc33e56f31020c
Djowood <jolan.cornevin@insa-lyon.fr>
2016-09-27 19:35:54 +0200

    add mock and game adaptater

--- app/src/main/java/ot3/insa/fr/geodraw/GamesListActivity.java
+++ app/src/main/java/ot3/insa/fr/geodraw/GamesListActivity.java
@@ -1,12 +1,21 @@
 package ot3.insa.fr.geodraw;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ot3.insa.fr.geodraw.models.Game;
 
 /**
  * Created by Djowood on 27/09/2016.
@@ -41,18 +50,77 @@ public class GamesListActivity extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.on_going_games, container, false);
 
-        mListView = (ListView) rootView.findViewById(R.id.listGames);
+        //Récupération de la liste des personnes
+        //TODO à remplacer par les données de la bd
+        ArrayList<Game> listG = Game.getMockGames();
 
-        //Création de l'adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(),
-                android.R.layout.simple_list_item_1, mStrings);
+        //Création et initialisation de l'Adapter pour les personnes
+        GameAdapter adapter = new GameAdapter(this.getContext(), listG);
 
-        mListView.setAdapter(adapter);
+        //Récupération du composant ListView
+        ListView list = (ListView)rootView.findViewById(R.id.listGames);
+
+        //Initialisation de la liste avec les données
+        list.setAdapter(adapter);
 
         return rootView;
+    }
+
+
+    public class GameAdapter extends BaseAdapter {
+        // Une liste de games
+        private List<Game> mListG;
+        //Le contexte dans lequel est présent notre adapter
+        private Context mContext;
+        //Un mécanisme pour gérer l'affichage graphique depuis un layout XML
+        private LayoutInflater mInflater;
+
+        public GameAdapter(Context context, List<Game> aListG) {
+            mContext = context;
+            mListG = aListG;
+            mInflater = LayoutInflater.from(mContext);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout layoutItem;
+            //(1) : Réutilisation des layouts
+            if (convertView == null) {
+                //Initialisation de notre item à partir du  layout XML "personne_layout.xml"
+                layoutItem = (LinearLayout) mInflater.inflate(R.layout.item_list_game, parent, false);
+            } else {
+                layoutItem = (LinearLayout) convertView;
+            }
+
+            //(2) : Récupération des TextView de notre layout
+            TextView game_name = (TextView)layoutItem.findViewById(R.id.gameName);
+            TextView game_nb_players = (TextView)layoutItem.findViewById(R.id.gameNbPlayers);
+            TextView game_time = (TextView)layoutItem.findViewById(R.id.gameTime);
+            TextView game_theme = (TextView)layoutItem.findViewById(R.id.gameTheme);
+
+            //(3) : Renseignement des valeurs
+            Game game = mListG.get(position);
+            game_name.setText(game.getName());
+            game_nb_players.setText(game.getCurrentNbPlayer() + " / " + game.getMaxNbPlayer());
+            game_time.setText(game.getCurrentTimeLeft() + " / " + game.getTotalTime());
+            game_theme.setText(game.getTheme());
+
+            //On retourne l'item créé.
+            return layoutItem;
+        }
+
+        public int getCount() {
+            return mListG.size();
+        }
+
+        public Object getItem(int position) {
+            return mListG.get(position);
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
     }
 }
--- app/src/main/java/ot3/insa/fr/geodraw/models/Game.java
+++ app/src/main/java/ot3/insa/fr/geodraw/models/Game.java
@@ -1,5 +1,6 @@
 package ot3.insa.fr.geodraw.models;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -8,23 +9,44 @@ import java.util.Date;
 
 public class Game {
     private String name;
+    private Boolean lock;
     private int currentNbPlayer;
     private int maxNbPlayer;
     private Date currentTimeLeft;
     private Date totalTime;
     private String theme;
 
-    public Game() {
-
-    }
-
-    public Game(String name, int currentNbPlayer, int maxNbPlayer, Date currentTimeLeft, Date totalTime, String theme) {
+    public Game(String name, Boolean lock, int currentNbPlayer, int maxNbPlayer, Date currentTimeLeft, Date totalTime, String theme) {
         this.name = name;
+        this.lock = lock;
         this.currentNbPlayer = currentNbPlayer;
         this.maxNbPlayer = maxNbPlayer;
         this.currentTimeLeft = currentTimeLeft;
         this.totalTime = totalTime;
         this.theme = theme;
+    }
+
+    public static ArrayList<Game> getMockGames() {
+        ArrayList<Game> listGames = new ArrayList<>();
+
+        listGames.add(new Game("Partie 1", false, 10, 20, new Date(2016, 01, 27, 00, 18, 00),
+                new Date(2016, 01, 27, 01, 00, 00), "Avion"));
+        listGames.add(new Game("Partie 2", false, 5, 5, new Date(2016, 01, 27, 5, 32, 00),
+                new Date(2016, 01, 27, 24, 00, 00), "Voiture"));
+        listGames.add(new Game("Partie 3", true, 5, 10, new Date(2016, 01, 27, 5, 32, 00),
+                new Date(2016, 01, 27, 24, 00, 00), ""));
+        listGames.add(new Game("Partie 4", true, 8, 20, new Date(2016, 01, 27, 5, 32, 00),
+                new Date(2016, 01, 27, 24, 00, 00), "Guerre"));
+
+        return listGames;
+    }
+
+    public Boolean getLock() {
+        return lock;
+    }
+
+    public void setLock(Boolean lock) {
+        this.lock = lock;
     }
 
     public String getName() {
