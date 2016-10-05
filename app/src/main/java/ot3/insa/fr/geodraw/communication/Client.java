00cfdf30a4f6ae8eaac1b3e0a585efe485939504
Franck Meyer <franck.meyer@insa-lyon.fr>
2016-10-05 09:44:21 +0200

    added message pool that should work

--- app/src/main/java/ot3/insa/fr/geodraw/communication/Client.java
+++ app/src/main/java/ot3/insa/fr/geodraw/communication/Client.java
@@ -5,6 +5,7 @@ import com.m5c.safesockets.SafeSocket;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import ot3.insa.fr.geodraw.communication.message.AddLatLng;
 import ot3.insa.fr.geodraw.communication.message.GameList;
@@ -25,10 +26,13 @@ public class Client extends Side
 	private final String ip;
 
 	private List<ClientListener> listeners;
+	private ConcurrentLinkedQueue<String> messagePool; 
 
 	public static Client theClient;
 
-
+	private Thread connector;
+	private Thread msgSender;
+	
 	private boolean isStopped;
 
 	static {
@@ -46,21 +50,43 @@ public class Client extends Side
 		this.isStopped = false;
 
 		breakdown.add(new ClientBreak());
+		
+		messagePool = new ConcurrentLinkedQueue<String>();
 
-		Thread t = new Thread() {
-			public void run(){
-				socket = null;
-				while (socket == null && !isStopped)
-					try {
-						socket = new SafeSocket(ip, port, HEART_BEAT_RATE, TIMEOUT, mess, breakdown);
-					} catch (IOException e) {
-						e.printStackTrace();
+		connect();
+
+		msgSender = new Thread("Message sender") {
+			
+			@Override
+			public void run() {
+				String msg;
+				boolean sent = true;
+				
+				while(!isStopped){
+					
+					if(sent){
+						sent = false;
+						msg = messagePool.poll();
 					}
+					
+					if(msg == null)
+					{
+						sent = true;
+						Thread.sleep(500);
+						continue;
+					}
+					
+					try {
+						if(socket != null)
+							if(socket.sendMessage(msg))
+								sent = true;
+					}
+				}
 			}
 		};
-		t.start();
-
-
+		
+		msgSender.start();
+		
 	}
 	
 
@@ -76,9 +102,18 @@ public class Client extends Side
 	public boolean sendMessage(Message m)
 	{
 		String jsonstr = Utils.gson.toJson(m);
-		if(socket == null)
+		
+		messagePool.offer(jsonstr);
+		
+		return true;
+		/*if(socket == null)
 			System.err.println("Error null socket!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		return socket.sendMessage(jsonstr);
+		return socket.sendMessage(jsonstr);*/
+	}
+	
+	private boolean sendMsg(String msg)
+	{
+		return socket.sendMessage(msg);
 	}
 	
 	public void disconnect()
@@ -88,13 +123,25 @@ public class Client extends Side
 			socket.disconnect();
 	}
 	
+	private void connect()
+	{
+		connector = new Thread("Connector") {
+			public void run(){
+				socket = null;
+				while (socket == null && !isStopped)
+					try {
+						socket = new SafeSocket(ip, port, HEART_BEAT_RATE, TIMEOUT, mess, breakdown);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			}
+		};
+		connector.start();
+	}
+	
 	private void reconnect()
 	{
-		try {
-			socket = new SafeSocket(ip, port, HEART_BEAT_RATE, TIMEOUT, mess, breakdown);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		connect();
 	}
 	
 	class ClientBreak implements BreakdownObserver
