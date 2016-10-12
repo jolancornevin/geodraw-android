64c8b15de189d06bd7a92339ba2334bf17b9e02a
Djowood <jolan.cornevin@insa-lyon.fr>
2016-10-12 11:52:09 +0200

    add cast LatLon to GoogleMap LatLon

--- app/src/main/java/ot3/insa/fr/geodraw/model/Segment.java
+++ app/src/main/java/ot3/insa/fr/geodraw/model/Segment.java
@@ -1,30 +1,38 @@
 package ot3.insa.fr.geodraw.model;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Represents a list of gps fixes top be drawn connected.
- * @author Max 
+ *
+ * @author Max
  */
 public class Segment {
     private final List<LatLng> segment;
 
-    public Segment()
-    {
+    public Segment() {
         this.segment = new LinkedList<>();
     }
 
-    public List<LatLng> getSegment()
-    {
+    public List<LatLng> getSegment() {
         return Collections.unmodifiableList(segment);
     }
-    
-    
-    public void addLatLng(LatLng latLng)
-    {
+
+
+    public void addLatLng(LatLng latLng) {
         segment.add(latLng);
-    }        
-    
+    }
+
+    public List<com.google.android.gms.maps.model.LatLng> getGoogleMapSegment() {
+        List<com.google.android.gms.maps.model.LatLng> googleMapSegment = new ArrayList<>();
+
+        for (LatLng latLon : this.getSegment()) {
+            googleMapSegment.add(latLon.getGoogleLatLon());
+        }
+
+        return googleMapSegment;
+    }
 }
